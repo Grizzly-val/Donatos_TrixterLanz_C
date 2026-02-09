36ff3c21fccaf0955f5c7b1fe60e131126ed46a0
Grizzly-val <lanz.10donatos@gmail.com>
2026-02-09 10:37:54 +0800

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter amount in USD: ");
+double usd = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter exchange rate from USD to EUR: ");
+double eRate = double.Parse(Console.ReadLine()!);
+
+double amount = usd * eRate;
+
+Console.Write("Amount in EUR: {0:F2}", amount);
---
+++ InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter principal amount: ");
+double p = double.Parse(Console.ReadLine()!);
+
+Console.Write("Enter rate of interest: ");
+float r = float.Parse(Console.ReadLine()!);
+Console.Write("Enter time in years: ");
+int years = int.Parse(Console.ReadLine()!);
+
+double simpleInterest = (p * r * years) / 100;
+
+Console.Write("Simple interest: {0:F2}", simpleInterest);
